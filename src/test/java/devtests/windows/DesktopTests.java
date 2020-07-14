d394a204ac7a26ef5cdbb1af0b9002207ce71250
Istvan Karsai <istvan.karsai.public@gmail.com>
2020-07-15 01:42:23 +0200

    Updating thorium-framework-core dependency and changing some testing things. Hopefully less clutter. (#8)

--- src/test/java/devtests/windows/DesktopTests.java
+++ src/test/java/devtests/windows/DesktopTests.java
@@ -1,25 +1,21 @@
 package devtests.windows;
 
+import constants.AssertionConstants;
 import devtests.windows.constants.DesktopConstants;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sikuli.script.Screen;
 
-public class DesktopTests {
+class DesktopTests {
     @DisplayName("Start button exists")
     @Test
     public void startButtonExists() {
-        final var result = DesktopConstants.START_BUTTON.get().get().apply(new Screen());
-        Assertions.assertTrue(result.status, result.message.toString());
+        AssertionConstants.assertDataTrue.accept(DesktopConstants.START_BUTTON.get().apply(new Screen()));
     }
 
     @DisplayName("IntelliJ text exists")
     @Test
-    public void intellijTextExists() throws InterruptedException {
-        final var screen = new Screen();
-        //screen.wait(30f);
-        final var result = DesktopConstants.INTELLIJ_TEXT.get().get().apply(screen);
-        Assertions.assertTrue(result.status, result.message.toString());
+    public void intellijTextExists() {
+        AssertionConstants.assertDataTrue.accept(DesktopConstants.INTELLIJ_TEXT.get().apply(new Screen()));
     }
 }
